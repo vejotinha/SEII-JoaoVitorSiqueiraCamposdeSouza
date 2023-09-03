f2dcca8eb21f7df8cf1a04e69d300744aa630c6d
João Vitor Siqueira <120279573+vejotinha@users.noreply.github.com>
2023-09-03 17:30:38 -0300

    Add files via upload

---
+++ Exercicio05/plot06.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = np.arange(-1000,1000,1)
+
+plt.plot(x1, x1**2)
+plt.show()
+
+x1 = np.arange(-1000,1000,1)
+
+plt.plot(x1, -x1**3+4)
+plt.show()
+
+
