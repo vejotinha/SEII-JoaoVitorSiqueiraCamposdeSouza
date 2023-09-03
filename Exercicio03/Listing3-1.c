67d5d4250cb0c953d2717d05d56ba94c8d79a06e
João Vitor Siqueira <120279573+vejotinha@users.noreply.github.com>
2023-09-03 17:38:54 -0300

    Delete Exercicio03 directory

--- Exercicio03/Listing3-1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main ()
-{
-    print("The process ID is %d\n", (int) getpid());
-    print("The parent process ID is %d\n", (int) getppid());
-    return 0;
-}
--- Exercicio03/Listing3-2.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-int main ()
-{
-    int return_value;
-    return_valeu = system("ls -l /");
-    return return_value;
-}
