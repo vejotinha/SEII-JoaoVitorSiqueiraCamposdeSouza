c192af9fd5649175bd7b204bcf7d0dabf70eae6d
João Vitor Siqueira <120279573+vejotinha@users.noreply.github.com>
2023-10-13 22:09:10 -0300

    Add files via upload

---
+++ Semana07/Calculadora/calculadora.js
@@ -0,0 +1,14 @@
+var resultado;
+var valor;
+function botao(num){
+    valor = document.calc.visor.value += num;
+}
+
+function reseta(){
+    document.calc.visor.value = '';
+}
+
+function calcula(){
+    resultado = eval(valor);
+    document.calc.visor.value = resultado.toLocaleString('pt-br');
+}
